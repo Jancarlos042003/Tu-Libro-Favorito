5fd8bc4287fcb5c7304df1ca5418d64c0998b3ca
Jancarlos042003 <U21302940@utp.edu.pe>
2024-10-28 00:56:42 -0500

    Importando la ruta de los componentes  al Router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,3 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Error404 from "../pages/Error404";
+import Home from "../pages/Home"
+import Detail from "../pages/Detail"
+import App from "../components/templates/App";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import AdminHome from "../pages/AdminHome"
+import Admin from "../components/templates/Admin";
+import CreateBook from "../components/form/CreateBook";
+import Inventory from "../pages/Inventory";
+
 const router = createBrowserRouter([
     {
         path: "/",
