c56572d481b9fa40be5e0d7c0e474be39f0eb7d9
Jancarlos042003 <U21302940@utp.edu.pe>
2024-11-12 02:45:17 -0500

    Se agregó comentarios

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -3,8 +3,12 @@ import { createContext, useEffect, useState } from "react";
 import { API_URL } from "../../env";
 import { token } from "../helpers/auth";
 
+// 1. CREAMOS EL CONTEXTO
+// ESTE ES EL QUE DEBEMOS DE CONSUMIR
 const UserContext = createContext()
 
+// 2. CREAER PROVIDER
+// ES EL QUE NOS PROVEE DE ACCESO A LOS DATOS DEL CONTEXTO
 const UserProvider = ({children}) => {
 
     const[userData, setUserData] = useState()
