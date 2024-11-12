4cc6f81742f38220c789bb9229941c1d4cfd92be
Jancarlos042003 <U21302940@utp.edu.pe>
2024-11-12 02:43:12 -0500

    Agregó el campo de disable.

--- src/components/review/SendButtom.jsx
+++ src/components/review/SendButtom.jsx
@@ -1,9 +1,10 @@
 import { Send } from 'lucide-react'
 
-const SendButton = ({text}) => {
+const SendButton = ({text, disabled=false}) => {
     return(
         <button 
-        className='bg-black text-white px-4 py-3 font-bold rounded-lg hover:bg-neutral-800 transition-colors duration-300 focus:outline-none text-sm flex justify-center w-full'
+        disabled={disabled}
+        className={`bg-black text-white px-4 py-3 font-bold rounded-lg ${disabled ? "hover:bg-neutral-600 cursor-not-allowed" : "hover:bg-neutral-800" } transition-colors duration-300 focus:outline-none text-sm flex justify-center w-full`}
         type='submit'
         >
             <Send size={18} className='mr-2' />
