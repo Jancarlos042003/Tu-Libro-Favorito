b70b0cf49a9e305bcd39df82909e373998f920d8
Jancarlos042003 <U21302940@utp.edu.pe>
2024-11-19 02:29:08 -0500

    Se agregó un botón que te direcciona a la página de ver libros por categoría.

--- src/components/recommendations/SectionBooks.jsx
+++ src/components/recommendations/SectionBooks.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom"
 import BookCarousel from "../carousel/BookCarousel"
 
-const SectionBooks = ( {titulo, libros} ) => {
+const SectionBooks = ( {titulo, libros, idCategoria} ) => {
     return(
-        <div className="w-auto h-auto my-12 px-2 md:px-0">
-            <h1 className="text-black font-bold mb-2 text-xl">{titulo}</h1>
+        <div className="w-auto h-auto my-20 px-2 xl:px-0">
+            <div className="flex justify-between mb-5">
+                <h1 className="text-black font-bold mb-2 text-2xl uppercase">{titulo}</h1>
+                <Link to={`/categoria/${idCategoria}`}>
+                    <button className="text-black text-sm uppercase font-bold px-5 py-2 rounded-lg bg-neutral-200 hover:bg-neutral-300">
+                        Ver más
+                    </button>
+                </Link>
+            </div>
             <BookCarousel books={libros} />
         </div>
     )
