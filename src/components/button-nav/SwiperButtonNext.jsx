13b3104deda5a97fd4ce179396d48fe0db97b121
Jancarlos042003 <U21302940@utp.edu.pe>
2024-11-27 01:50:37 -0500

    Se agregó el titulo a los botones de Prev y Next

--- src/components/button-nav/SwiperButtonNext.jsx
+++ src/components/button-nav/SwiperButtonNext.jsx
@@ -3,7 +3,11 @@ import { useSwiper } from 'swiper/react';
 const SwiperButtonNext = ({ children }) => {
     const swiper = useSwiper();
     return (
-        <button onClick={() => swiper.slideNext()} className="absolute right-1 top-1/2 z-10 -translate-y-1/2 bg-transparent">
+        <button 
+        onClick={() => swiper.slideNext()} 
+        className="absolute right-1 top-1/2 z-10 -translate-y-1/2 bg-transparent"
+        title='Siguiente'
+        >
             {children}
         </button>
     );
--- src/components/button-nav/SwiperButtonPrev.jsx
+++ src/components/button-nav/SwiperButtonPrev.jsx
@@ -3,7 +3,11 @@ import { useSwiper } from 'swiper/react';
 const SwiperButtonPrev = ({ children }) => {
     const swiper = useSwiper();
     return (
-        <button onClick={() => swiper.slidePrev()} className="absolute left-1 top-1/2 z-10 -translate-y-1/2 bg-transparent">
+        <button 
+        onClick={() => swiper.slidePrev()} 
+        className="absolute left-1 top-1/2 z-10 -translate-y-1/2 bg-transparent"
+        title='Anterior'
+        >
             {children}
         </button>
     );
