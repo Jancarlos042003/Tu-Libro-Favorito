7b62aca23a171de4dfe45a27034123c744009a6c
Jancarlos042003 <U21302940@utp.edu.pe>
2024-11-28 22:37:12 -0500

    Se creo un contexto para el token y las funciones de login y logout

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 import { API_URL } from "../../env";
-import { token } from "../helpers/auth";
+import { deleteToken, setToken, token } from "../helpers/auth";
 
 // 1. CREAMOS EL CONTEXTO
 // ESTE ES EL QUE DEBEMOS DE CONSUMIR
@@ -11,24 +11,39 @@ const UserContext = createContext()
 // ES EL QUE NOS PROVEE DE ACCESO A LOS DATOS DEL CONTEXTO
 const UserProvider = ({children}) => {
 
-    const[userData, setUserData] = useState()
+    const[userData, setUserData] = useState(null)
+    const [userToken, setUserToken] = useState(token()); // Estado para el token
 
     useEffect(() =>{
-        if(token()){
+        if(userToken){
             axios.get(`${API_URL}/api/usuario/me`, {
                 headers: {
-                    Authorization: `Bearer ${token()}`
+                    Authorization: `Bearer ${userToken}`
                 }
             })
             .then(resp => {
                 setUserData(resp.data)
             })
+            .catch(() => setUserData(null));
+        } else {
+            setUserData(null); // Si no hay token, limpiamos los datos del usuario
         }
-    },[])
+    },[userToken]);
+
+    const login = (token) => {
+        setToken(token); // Guardar en almacenamiento local
+        setUserToken(token); // Actualizar estado local
+    };
+
+    const logout = () => {
+        deleteToken(); // Eliminar de almacenamiento local
+        setUserToken(null); // Actualizar estado local
+        setUserData(null); // Limpiar datos de usuario
+    };
 
     // Value -> valor que se le pasara como contexto a los componentes hijos 
     return(
-        <UserContext.Provider value={{userData, setUserData}}>
+        <UserContext.Provider value={ { userData, login, logout } }>
             {children}
         </UserContext.Provider>
     )
