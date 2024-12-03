24e94fd2f5f741b488f12695bcbc5caabf1b15d9
Jancarlos042003 <U21302940@utp.edu.pe>
2024-12-03 07:37:07 -0500

    Definimos el layout que mantendrá el footer siempre abajo

--- src/components/templates/App.jsx
+++ src/components/templates/App.jsx
@@ -2,11 +2,14 @@ import { Outlet } from "react-router-dom"
 import Header from "../header/Header"
 import Footer from "../footer/Footer"
 
+// Definimos el layout que mantendrá el footer siempre abajo
 const App = () => {
     return(
-        <div>
+        <div className="flex flex-col min-h-screen">
             <Header />
-            <Outlet />
+            <main className="flex-grow">
+                <Outlet />
+            </main>
             <Footer />
         </div>
     )
