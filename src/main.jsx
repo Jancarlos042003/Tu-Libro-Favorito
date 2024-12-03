3ecbcb8f1ef63a51b27bc5e83343559e2db38aa5
Jancarlos042003 <U21302940@utp.edu.pe>
2024-12-03 07:37:58 -0500

    Añadimos QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,17 @@ import router from './router/Router'
 import './index.css'
 import { CartProvider } from './context/CartContext'
 import { UserProvider } from './context/UserContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+// Crear una instancia de QueryClient
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  <UserProvider>
-    <CartProvider>
-      <RouterProvider router={router} future={{ v7_startTransition: true }} />
-    </CartProvider>
-  </UserProvider>
+  <QueryClientProvider client={queryClient}>
+    <UserProvider>
+      <CartProvider>
+        <RouterProvider router={router} future={{ v7_startTransition: true }} />
+      </CartProvider>
+    </UserProvider>
+  </QueryClientProvider>
 )
